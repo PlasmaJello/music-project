edf09c66f7b9dda834d6df16d5b1eb5007c89484
Sina <sinafard1381@gmail.com>
2026-06-17 15:32:06 +0100

    isolating the beats

---
+++ jungle_remix.py
@@ -0,0 +1,12 @@
+from pydub import AudioSegment
+
+# 1. Load your clean sample
+sample = AudioSegment.from_wav("cool_down_pace_sample.wav")
+
+# 2. Slice out 4 distinct beats based on detect.py
+beat1 = sample[0:400]  # From 0ms to the first hit
+beat2 = sample[400:800]  # From 400ms to the second hit
+beat3 = sample[800:1150]  # From 800ms to the third hit
+beat4 = sample[1150:1450]  # From 1150ms to the fourth hit
+
+print("Chops successfully created!")
